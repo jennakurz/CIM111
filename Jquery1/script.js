9414480e395ffe188def0c5bcd3290b27b7152e1
Jenna Kurz <jxk682@miami.edu>
2018-04-02 13:42:44 -0400

    updated how to

--- Jquery1/script.js
+++ Jquery1/script.js
@@ -26,20 +26,35 @@ $(document).ready(function(){
 
   $(document).ready(function(){
 
-        $("#hide").click(function(){
-          $(".instructions").hide();
+    $("#show2").click(function(){
+        $("#instructions").show();
+      });
+
+        $("#hide2").click(function(){
+          $("#instructions").hide();
         });
 
-       $("#show").click(function(){
-           $(".instructions").show();
-         });
+
 
      });
 
 
   $(document).ready(function(){
-$("toggle").toggle(function(){
-  $(".image").toggle(1000);
+$("#toggle").click(function(){
+  $("#finalcookies").toggle(2000);
 });
 
+
+$("#toggle").click(function(){
+  $("#finalcookies").show();
+});
+
+$("#finalcookies").hover(function(){
+  $("#replacetext").show();
+});
+});
+
+
+$("#title").hover(function(){
+  $("#title").css("color", "brown");
 });
